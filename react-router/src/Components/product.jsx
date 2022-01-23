107d7a6a986aaf77c1fedf5e676b6c1b13185ef0
AMIT YADAV <er.itechamit@gmail.com>
2022-01-23 11:51:50 +0530

    fixed changing to next single products

--- react-router/src/Components/product.jsx
+++ react-router/src/Components/product.jsx
@@ -16,26 +16,27 @@ export const Mydataprovider=({children})=>{
 
 export const Product=()=>{
     const[productdata,setProductdata] = useState([])
+    const [data,setData] = useState(null)
 
     useEffect(()=>{
         fetch('https://fakestoreapi.com/products')
         .then((p)=>p.json())
         .then((data)=>setProductdata(data))
+        localStorage.setItem('singleproduct',JSON.stringify([]))
     },[]) 
+
 
     return<>
         <h3>This is products page</h3>
         <div className="displaybox">
             <>{productdata.map((el)=>
             <div >
-
                 <div id="imgbox"><img src={el.image} /></div>
                 <div className="textbox">
                     <div><h5>{el.title}</h5></div>
                     <div>{el.category}</div>
                     <div style={{color:"darkred"}}>${el.price}</div>
-                    <Link to={`/product/${el.id}`}>Moredetail</Link>
-                    <>{()=>{localStorage.setItem('cartvalue',JSON.stringify([]))}}</>               
+                    <Link to={`/product/${el.id}`} onClick={()=>{localStorage.setItem('singleproduct',JSON.stringify([el]))}}>Moredetail</Link>             
                 </div>
             </div>
             )}</>
--- react-router/src/Components/singleproduct.jsx
+++ react-router/src/Components/singleproduct.jsx
@@ -4,9 +4,21 @@ import { Mydata } from "./product"
 
 
 export const Single=()=>{
-    var obj = JSON.parse(localStorage.getItem('cartvalue'))
-    console.log(obj)
+    var arr = JSON.parse(localStorage.getItem('singleproduct'))
     return<>
-
+        <div>this is single page</div>
+        <div className="displaybox2">
+            <>{arr.map((el)=>
+            <div>
+                <div id="imgbox2"><img src={el.image} /></div>
+                <div className="textbox">
+                    <div><h5>{el.title}</h5></div>
+                    <div>{el.category}</div>
+                    <div><p>{el.description}</p></div>
+                    <div style={{color:"darkred"}}>${el.price}</div>           
+                </div>
+            </div>
+            )}</>
+        </div>
     </>
 }
